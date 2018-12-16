f411dce8ded103f39364500adbca6554f23b5cc5
Bartosz Legięć <bartosz@legiec.eu>
2018-12-16 21:31:46 +0100

    Remove sass

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'reset-css';
-import 'src/styles/index.scss';
+import 'src/styles/index.css';
 
 import Root from 'src/components/Root/Root';
 
